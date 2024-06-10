18ebcc22301cf6ecdc18983a8bc7a3c2bfe7205b
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2024-06-10 19:47:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import constants from "./constants";
+
+function App() {
+  const [idx, setIdx] = useState(null);
+  return (
+    <main>
+      <div className="container">
+        <div className="heading">
+          <img src="/icon-star.svg" alt="star Icon" className="star-img" />
+          <p className="heading-title ">FAQs</p>
+        </div>
+        {constants.map((item) => (
+          <div
+            className="question"
+            key={item.id}
+            onClick={() => setIdx(idx === item.id ? null : item.id)}
+          >
+            <div className="question-title">
+              <p style={{ fontWeight: "700", maxWidth: "90%" }}>{item.que}</p>
+              {idx === item.id ? (
+                <img
+                  src="/icon-minus.svg"
+                  alt="arrow Icon"
+                  className="arrow-img"
+                  onClick={() => setIdx(null)}
+                />
+              ) : (
+                <img
+                  src="/icon-plus.svg"
+                  alt="arrow Icon"
+                  className="arrow-img"
+                  onClick={() => setIdx(item.id)}
+                />
+              )}
+            </div>
+            <div className="question-ans">
+              {idx === item.id && <p>{item.ans}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,28 @@
+const constants = [
+  {
+    id: 1,
+    que: "What is Frontend Mentor, and how will it help me?",
+    ans:
+      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+  },
+  {
+    id: 2,
+    que: "Is Frontend Mentor free?",
+    ans:
+      "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+  },
+  {
+    id: 3,
+    que: "Can I use Frontend Mentor projects in my portfolio?",
+    ans:
+      "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellentway to showcase your skills to potential employers!",
+  },
+  {
+    id: 4,
+    que: "  How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    ans:
+      "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+  },
+];
+
+export default constants;
